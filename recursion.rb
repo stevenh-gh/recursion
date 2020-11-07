6c3744ad251242380ce69dc833994734c7827547
steven huynh <steven.h@live.com>
2020-11-07 15:14:00 -0800

    added recursive fibs

--- recursion.rb
+++ recursion.rb
@@ -1,5 +1,18 @@
 def fibs(num)
+  return [] if num == 0
+  return [1] if num == 1
+
   arr = [1, 1]
   arr << arr[arr.length - 2] + arr[arr.length - 1] until arr.length == num
   arr
 end
+
+def fibs_rec(num)
+  return [] if num == 0
+  return [1] if num == 1
+  return [1, 1] if num == 2
+
+  fibs_rec(num - 1).push(fibs_rec(num - 1)[-1] + fibs_rec(num - 1)[-2])
+end
+
+p fibs_rec(99)
